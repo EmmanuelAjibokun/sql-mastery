f39a3efb20c8e70360fc188917b95d7b595e3a98
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-09-20 08:35:31 +0100

    business question

--- 8 weeks of sql challenge/dannys_diner_queries.sql
+++ 8 weeks of sql challenge/dannys_diner_queries.sql
@@ -142,6 +142,8 @@ FROM (
 GROUP BY customer_id
 ORDER BY customer_id;
 
+-- In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
 
 SELECT * FROM all_tables;
     
